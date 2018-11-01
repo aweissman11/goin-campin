050f411de2cfdb423693dddae4672b077e7e95c3
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-01 16:45:54 -0600

    getting started

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getCurrentLocation } from ''
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getCurrentLocation();
+  }
+
   render() {
     return (
       <div className="App">
         <div className='intro-animation-container'>
         </div>
-        <body>
-          <header>We're Goin Campin!</header>
+        <section>
+          <header>We're Goin Hiking!</header>
           <div>
-            Search here
+            <input placeholder='search here' />
           </div>
           <div>
             <h3>Results:</h3>
             <div>
               <ul>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
-                <li>Campground Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
+                <li>Hike Name</li>
               </ul>
             </div>
           </div>
-        </body>
+        </section>
+        <section>
+
+        </section>
       </div>
     );
   }
 }
 
-export default App;
+export const mapDispatchToProps = (dispatch) => {
+  () => dispatch(getCurrentLocation())
+}
+
+export default connect(null, mapDispatchToProps)(App);
