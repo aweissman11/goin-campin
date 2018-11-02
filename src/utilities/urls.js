e813689e2a970dc77a2e58be084f1e2ecfe7cabd
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-02 11:31:25 -0600

    create hikingList url

---
+++ src/utilities/urls.js
@@ -0,0 +1,8 @@
+import { hikingKey } from '../apiKeys';
+
+export const hikingListUrl = (location) => {
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&lon=${location[1]}&maxDistance=10&key=${hikingKey}`
+  return url
+}
+
+
