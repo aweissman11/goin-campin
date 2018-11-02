21708faf644877e0ecb8f9b776d33d8b69f6a84e
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-02 10:32:53 -0600

    getCurrentLocationThunk working

--- src/actions/getCurrentLocationThunk.js
+++ src/actions/getCurrentLocationThunk.js
@@ -2,6 +2,6 @@ import { setCurrentLocation } from './index';
 
 export const getCurrentLocation = (location) => {
   return async (dispatch) => {
-      dispatch(setCurrentLocation(location || []));
+    dispatch(setCurrentLocation(location));
   }
 }
