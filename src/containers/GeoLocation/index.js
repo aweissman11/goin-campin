849b88e80701f8c95b19f227828c1542d73e998f
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-02 10:33:35 -0600

    grab location without displaying anything

--- src/containers/GeoLocation/index.js
+++ src/containers/GeoLocation/index.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {geolocated} from 'react-geolocated';
 
 import './GeoLocation.css';
 
-class GeoLocation extends React.Component {
+
+class GeoLocation extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      latitude: 0,
+      longitude: 0
+    }
+  }
+
   componentDidUpdate() {
     if (this.props.isGeolocationEnabled) {
-      const { latitude, longitude } = this.props.coords
-      console.log('latitude:', latitude);
-      this.props.setInitialLocation([latitude, longitude])
+      if (this.state.latitude !== this.props.coords.latitude) {
+        const { latitude, longitude } = this.props.coords;
+        console.log('latitude:', latitude);
+        this.props.setInitialLocation([latitude, longitude])
+        this.setState({
+          latitude,
+          longitude
+        })
+      } else {
+        return;
+      }
+    } else {
+      return;
     }
   }
 
   render() {
-    return !this.props.isGeolocationAvailable
-      ? <div>Your browser does not support Geolocation</div>
-      : !this.props.isGeolocationEnabled
-        ? <div>Geolocation is not enabled</div>
-        : this.props.coords
-          ? <div className='location-data'>
-            <h3>Your current latitude:</h3>
-            <p>{this.props.coords.latitude}</p>
-            <h3>Your current longitude:</h3>
-            <p>{this.props.coords.longitude}</p>
-          </div>
-          : <div>Getting the location data&hellip; </div>;
+    return (
+      <div name='placeholder-div--ignore'></div>
+    )
   }
 }
 
