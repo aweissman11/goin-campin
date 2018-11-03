3bb26ee2078ace3e0067f48a691606e52214e265
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-03 15:35:23 -0600

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import { currentLocation } from './currentLocation';
 import { hikesList } from './hikesList';
 import { campsList } from './campsList';
+import { loading } from './loading';
 
 export const rootReducer = combineReducers({
   currentLocation,
   hikesList,
-  campsList
+  campsList,
+  loading
 })
