a04cf1fab3cb8240b89c6fca15d2ad4d3a14fccb
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-03 15:34:48 -0600

    refactor

--- src/containers/CampsList/index.js
+++ src/containers/CampsList/index.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Loading from '../../components/Loading';
+import LocationSearchInput from '../LocationSearchInput';
 import { getCampsList } from '../../actions/getCampgroundsThunk';
 
 class CampsList extends Component {
-
-  getCampsList = () => {
-    const { currentLocation, campsList } = this.props;
-    if (currentLocation.length > 0 && campsList.length < 1) {
-      this.props.getCampsList(currentLocation);
-    }
-  }
-
   renderCamps = () => {
-    this.getCampsList();
-    if (this.props.campsList.length > 1) {
-      const slicedList = this.props.campsList.slice(2, 12);
+    if (this.props.campsList.length > 0) {
+      const slicedList = this.props.campsList;
+      console.log('slicedList:', slicedList);
       return slicedList.map( camp => {
         if (camp.attributes.facilityName !== 'Rent an RV for Your Next Adventure') {
           return (
@@ -36,19 +30,36 @@ class CampsList extends Component {
     }
   }
 
+  
+
   render() {
     return (
-      <div className='hikes-list'>
-        <h1>CampsList</h1>
-          {
-            this.renderCamps()
-          }
+    <div className='camps-list'>
+      <header>We're Goin Campin!</header>
+      <div>
+        <LocationSearchInput 
+          getCampsList={this.props.getCampsList}
+        />
       </div>
+      {
+
+      this.props.loading.isLoading ?
+      <Loading
+        message={this.props.loading.message}
+      /> :
+        <div>
+          <h1>CampsList</h1>
+            {
+              this.renderCamps()
+            }
+        </div>
+      }
+    </div>
     )
   }
 }
 
-export const mapStateToProps = ({ currentLocation, campsList }) => ({ currentLocation, campsList })
+export const mapStateToProps = ({ currentLocation, campsList, loading }) => ({ currentLocation, campsList, loading })
 
 export const mapDispatchToProps = (dispatch) => ({
   getCampsList: (location) => dispatch(getCampsList(location))
