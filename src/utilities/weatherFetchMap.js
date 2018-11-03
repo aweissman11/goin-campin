7ac5a03da2bf957009c14c435de0883a90b1acc3
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-03 08:35:19 -0600

    cleanup and comment out fetch for now

--- src/utilities/weatherFetchMap.js
+++ src/utilities/weatherFetchMap.js
@@ -1,9 +1,8 @@
-import fetchCall from '../utilities/fetchCall';
-import * as urls from '../utilities/urls';
-
 export const weatherFetchMap = async (camps) => {
   const weatherCamps = await camps.map( async camp => {
-    const currentWeather = await fetchCall(urls.currentWeatherUrl([camp.latitude, camp.longitude]))
+    // commented out to stop making fetch Calls
+    // const currentWeather = await fetchCall(urls.currentWeatherUrl([camp.latitude, camp.longitude]))
+    const currentWeather = { time: 45345345, condition: 'sunny' }
     return await { ...camp, currentWeather }
   })
   return Promise.all(weatherCamps);
