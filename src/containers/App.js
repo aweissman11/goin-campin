bd008d18c7dbd65ba3fd632258f70fba606f8a71
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-03 15:34:34 -0600

    refactor

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { loading } from '../actions/index'
 import { getCurrentLocation } from '../actions/getCurrentLocationThunk';
 import { getCampsList } from '../actions/getCampgroundsThunk';
-import GeoLocation from './GeoLocation';
-import Opening from './Opening';
+import Opening from '../components/Opening';
 import CampsList from './CampsList';
-import LocationSearchInput from './LocationSearchInput';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -23,59 +22,51 @@ class App extends Component {
   }
 
   completeOpening = () => {
-    console.log('complete')
     this.setState({ isOpening: false })
   }
 
-  setLocation = (location) => {
-    this.props.setInitialLocation(location)
+  setLocation = async (location) => {
+    await this.props.setInitialLocation(location)
+    await this.getCampsList();
+  }
+
+  getCampsList = () => {
+    const { currentLocation, campsList } = this.props;
+    if (currentLocation.length > 0 && campsList.length < 1) {
+      this.props.getCampsList(currentLocation);
+    }
   }
 
   render() {
     return (
       <Route to='/' render={() => {
        return (
-            <div className="App">
-              {
-                this.state.isOpening ?
-                  <Route to='/opening' render={() => 
-                    <Opening
-                      completeOpening={this.completeOpening}
-                    />
-                  } /> :
-                  <Route to='/home' render={()=> {
-                    return (
-                      <section>
-                        <header>We're Goin Campin!</header>
-                        <div>
-                          <LocationSearchInput 
-                            getCampsList={this.props.getCampsList}
-                          />
-                        </div>
-                          <GeoLocation
-                            setInitialLocation={(location) => this.setLocation(location)}
-                          />
-                        <div>
-                          <h3>Results:</h3>
-                        </div>
-                        <CampsList />
-                      </section>
-                    )
-                  }} />
-                }
-            </div>
-            )
+        <div className="App">
+          {
+            this.state.isOpening ?
+              <Route to='/opening' render={() => 
+                  <Opening
+                    completeOpening={this.completeOpening}
+                    setInitialLocation={(location) => this.setLocation(location)}
+                    geoLoading={(locationData) => this.props.geoLoading(locationData)}
+                  />
+              } /> :
+              <Route to='/home' component={CampsList} />
+            }
+          </div>
+        )
         }} />
             
     );
   }
 }
 
-export const mapStateToProps = ({ currentLocation, hikesList }) => ({ currentLocation, hikesList });
+export const mapStateToProps = ({ currentLocation, campsList }) => ({ currentLocation, campsList });
 
 export const mapDispatchToProps = (dispatch) => ({
   setInitialLocation: (location) => dispatch(getCurrentLocation(location)),
-  getCampsList: (location) => dispatch(getCampsList(location))
+  getCampsList: (location) => dispatch(getCampsList(location)),
+  geoLoading: (message, isLoading) => dispatch(loading(message, isLoading))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
