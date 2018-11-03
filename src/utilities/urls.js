8a4a43a90452a681aa83fbcea89e42d591fad04a
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-02 19:09:38 -0600

    weather url

--- src/utilities/urls.js
+++ src/utilities/urls.js
@@ -1,8 +1,11 @@
-import { hikingKey } from '../apiKeys';
+import { hikingKey, weatherKey } from '../apiKeys';
 
 export const hikingListUrl = (location) => {
   const url = `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&lon=${location[1]}&maxDistance=10&key=${hikingKey}`
-  return url
+  return url;
 }
 
-
+export const currentWeatherUrl = (location) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${weatherKey}`;
+  return url;
+}
