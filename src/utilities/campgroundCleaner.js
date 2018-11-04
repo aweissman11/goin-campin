1fb670f0b01fc3eeef2896503ea799b47f694ce0
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-04 09:54:31 -0700

    clean incoming data for campgrounds

---
+++ src/utilities/campgroundCleaner.js
@@ -0,0 +1,34 @@
+export const campgroundCleaner = (camps) => {
+  const cleanCamps = camps.map( camp => {
+    return {
+      name: camp.attributes.facilityName,
+      photo: `http://www.reserveamerica.com${camp.attributes.faciltyPhoto}`,
+      lat: camp.attributes.latitude,
+      long: camp.attributes.longitude,
+      wAmps: camp.attributes.sitesWithAmps,
+      wPets: camp.attributes.sitesWithPetsAllowed,
+      wSewer: camp.attributes.sitesWithSewerHookup,
+      wHose: camp.attributes.sitesWithWaterHookup,
+      wWater: camp.attributes.sitesWithWaterfront,
+      forecast: cleanForecast(camp.forecast.list)
+    }
+  })
+  return cleanCamps;
+}
+
+export const cleanForecast = (forecastList) => {
+  const fortyWeatherChecks = forecastList.map( check => {
+    return {
+      date: check.dt_txt,
+      humidity: check.main.humidity,
+      temp: check.main.temp,
+      max: check.main.temp_max,
+      min: check.main.temp_min,
+      descrip: check.weather[0].description,
+      summary: check.weather[0].main,
+      icon: `http://openweathermap.org/img/w/${check.weather[0].icon}.png`,
+      windSpeed: check.wind.speed
+    }
+  })
+  return fortyWeatherChecks
+}
