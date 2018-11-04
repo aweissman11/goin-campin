947d6ced61159f6fd3531fc1772b0d27dcfc3b12
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-04 14:15:13 -0700

    more cleanup

--- src/actions/getCurrentLocationThunk.js
+++ src/actions/getCurrentLocationThunk.js
@@ -6,10 +6,8 @@ export const getCurrentLocation = () => {
   return (dispatch) => {
     dispatch(loading('searching for a location...', true))
     try {
-      let retrievedLocation;
-      const gotcha = navigator.geolocation.getCurrentPosition((location) =>  {
+      navigator.geolocation.getCurrentPosition((location) =>  {
         const { latitude, longitude } = location.coords;
-        retrievedLocation = [latitude, longitude];
         dispatch(setCurrentLocation([latitude, longitude]));
         dispatch(getCampsList([latitude, longitude]))
       });
