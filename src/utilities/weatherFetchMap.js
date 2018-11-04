312af84c9b8e951a6736161676b22c773a15bd39
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-04 09:56:09 -0700

    cleanup

--- src/utilities/weatherFetchMap.js
+++ src/utilities/weatherFetchMap.js
@@ -5,7 +5,7 @@ export const weatherFetchMap = async (camps) => {
   const weatherCamps = await camps.map( async camp => {
     const { latitude, longitude } = camp.attributes
     const forecast = await fetchCall(urls.fiveDayUrl([latitude, longitude]))
-    return await { ...camp, currentWeather, forecast }
+    return await { ...camp, forecast }
   })
   return Promise.all(weatherCamps);
 }
