c04e658d8003a92a4c3a7f0ef8f467b556710a76
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-05 15:27:47 -0700

    test campgrounds fetch

---
+++ src/utilities/__tests__/campgroundsFetch.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import campgroundsFetch from '../campgroundsFetch';
+
+describe('campgroundsFetch', () => {
+  let mockUrl;
+
+  beforeEach(() => {
+    mockUrl = ''
+  })
+
+
+  it('should call fetch w/ the correct params', async () => {
+    window.fetch = jest.fn().mockImplementation(() => (
+      Promise.resolve({ text: () => Promise.resolve('Promise resolved') })
+    ))
+
+    await campgroundsFetch(mockUrl);
+    expect (window.fetch).toHaveBeenCalledWith(`https://cors-anywhere.herokuapp.com/`);
+  })
+
+  it('should return the response', async () => {
+    window.fetch = jest.fn().mockImplementation(() => (
+      Promise.resolve({ ok: true, text: () => Promise.resolve('Promise resolved') })
+    ))
+
+    const result = await campgroundsFetch(mockUrl);
+    expect(result).toEqual('Promise resolved')
+  })
+
+  it('should throw an error if failed', async () => {
+    window.fetch = jest.fn().mockImplementation(() => (
+      Promise.resolve({ ok: false })
+    ))
+
+    const result = await campgroundsFetch(mockUrl);
+
+    expect(result).toEqual(Error())
+    
+  })
+  
+})
