0537e9b046ca93f01b6549c9afbe60a8b4cb30fa
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-05 15:25:58 -0700

    test campslist

---
+++ src/containers/CampsList/__tests__/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CampsList, mapStateToProps } from '../index';
+import { mockCampsList } from '../__mocks__/mockCampsList';
+
+describe('CampsList', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CampsList 
+      campsList={mockCampsList}
+      loading={{isLoading: false, message: 'hi'}}
+    />)
+  })
+  
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('should match the snapshot if Loading', () => {
+    wrapper.instance().setState({ isLoading: true })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  
+  describe('mapStateToProps', () => {
+    it('should map the campslist state to props', () => {
+      const expected = mockCampsList
+      const mockState = {
+        campsList: mockCampsList
+      }
+      
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps.campsList).toEqual(expected)
+    })
+
+    it('should map the loading state to props', () => {
+      const expected = {
+        isLoading: true,
+        message: 'loading right now'
+      }
+      const mockState = {
+        loading: expected
+      }
+      
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps.loading).toEqual(expected)
+    })
+  })
+  
+})
