86365d57a732a9504123e20f45bbbb0f8ccbe52a
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-06 17:45:12 -0700

    fancy function

--- src/actions/getCampWeatherThunk.js
+++ src/actions/getCampWeatherThunk.js
@@ -2,15 +2,13 @@ import { setCampsList } from './index';
 import { campgroundCleaner } from '../utilities/campgroundCleaner'
 import { weatherFetchMap } from '../utilities/weatherFetchMap';
 
-export const getCampWeather = (camps) => {
-  return async (dispatch) => {
-    try {
-      const weatherCamps = await weatherFetchMap(camps)
-      const cleanCamps = campgroundCleaner(weatherCamps);
-      dispatch(setCampsList(cleanCamps));
-    } catch(error) {
-      console.warn(error.message);
-      return (error.message);
-    }
+export const getCampWeather = (camps) => async (dispatch) => {
+  try {
+    const weatherCamps = await weatherFetchMap(camps)
+    const cleanCamps = campgroundCleaner(weatherCamps);
+    dispatch(setCampsList(cleanCamps));
+  } catch (error) {
+    console.warn(error.message);
+    return (error.message);
   }
 }
