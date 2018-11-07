9d6dc9bdd8522d6b2e0d5542693e2e64c330fdbc
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-06 22:48:21 -0700

    mock data

---
+++ src/actions/__mocks__/mockCamps.js
@@ -0,0 +1,82 @@
+export const mockCamps = [{
+  "type": "element",
+  "name": "result",
+  "attributes": {
+    "agencyIcon": "",
+    "agencyName": "",
+    "availabilityStatus": "Y",
+    "contractID": "IND2",
+    "contractType": "PRIVATE",
+    "facilityID": "720142",
+    "facilityName": "Rent an RV for Your Next Adventure",
+    "faciltyPhoto": "/webphotos/IND2/pid720142/0/80x53.jpg",
+    "favorite": "N",
+    "latitude": "39.7302833",
+    "listingOnly": "Y",
+    "longitude": "-104.9525917",
+    "regionName": "",
+    "reservationChannel": "Web Reservable",
+    "shortName": "M042",
+    "sitesWithAmps": "Y",
+    "sitesWithPetsAllowed": "Y",
+    "sitesWithSewerHookup": "N",
+    "sitesWithWaterHookup": "N",
+    "sitesWithWaterfront": "Lakefront",
+    "state": "CO"
+  },
+  "forecast": {
+    "list": [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  "main": {
+    "humidity": 45,
+    "temp": 55,
+    "temp_max": 75,
+    "temp_min": 33
+  }
+},
+{
+  "type": "element",
+  "name": "result",
+  "attributes": {
+    "agencyIcon": "",
+    "agencyName": "",
+    "availabilityStatus": "N",
+    "contractID": "CO",
+    "contractType": "STATE",
+    "facilityID": "50021",
+    "facilityName": "Chatfield State Park",
+    "faciltyPhoto": "/webphotos/CO/pid50021/0/80x53.jpg",
+    "favorite": "N",
+    "latitude": "39.5380556",
+    "listingOnly": "N",
+    "longitude": "-105.0872222",
+    "regionName": "",
+    "reservationChannel": "",
+    "shortName": "CHSR",
+    "sitesWithAmps": "Y",
+    "sitesWithPetsAllowed": "Y",
+    "sitesWithSewerHookup": "Y",
+    "sitesWithWaterHookup": "Y",
+    "sitesWithWaterfront": "",
+    "state": "CO"
+  },
+  "forecast": {
+    "list": [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  "main": {
+    "humidity": 45,
+    "temp": 55,
+    "temp_max": 75,
+    "temp_min": 33
+  }
+}
+];
+
