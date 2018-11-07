cd89006947c4212b027615676a6f278c84713201
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-06 19:42:42 -0700

    no more console.warns

--- src/utilities/fetchCall.js
+++ src/utilities/fetchCall.js
@@ -1,7 +1,6 @@
 export const fetchCall = async (url) => {
   const response = await fetch(url);
   if (!response.ok) {
-    console.error(Error(response.statusText));
     return Error(response.statusText)
   }
   return await response.json();
